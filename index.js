44f55d52418ac3a2c460f9d5625b8dc1fc59f0e9
anasam31 <navratil.ana@gmail.com>
2024-02-03 16:04:14 +0100

    added AI api

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
+function displayHaiku(response) {
+  new Typewriter("#haiku", {
+    strings: response.data.answer,
+    autoStart: true,
+    cursor: null,
+    delay: 20,
+  });
+}
+
 function generateHaiku(event) {
   event.preventDefault();
 
-  new Typewriter("#haiku", {
-    strings: "An old silent pond",
-    autoStart: true,
-    cursor: null,
-    delay: 80,
-  });
+  let apiKey = "5dfd50a97fe35994et0fob99445b78ca";
+  let context =
+    "You are a haiku writer. Provide the haiku in the basic HTML format. Font size should be 18px, line height 1.5, and margin top 30px. Each line of haiku should be written in a separate line using <br/> and no bullet points. Include only the haiku. Don't write the title.";
+  let keyWord = document.querySelector("#key-word");
+  let prompt = `Write a haiku using the given word. Make sure you use the exact given word in the haiku. The given word is ${keyWord.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayHaiku);
 }
 
 let haikuFormElement = document.querySelector("#haiku-generator-form");
